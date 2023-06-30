160c5957203f6a9d76e6671c0533c143cd94c3bc
ArjunSahlot <iarjun.sahlot@gmail.com>
2023-06-29 19:06:02 -0700

    netlify redirect

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1c1aa48fdd346b43"; }
+/******/ 		__webpack_require__.h = function() { return "afbaa15d7a074402"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
